c5e00aaaa7dba5e987ec20b6fc1e311a42841f12
gongjiwon <rag9008@naver.com>
2019-01-22 16:57:43 +0900

    작품 등록 화면 구현

---
+++ BeBrav/ArtAddViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ArtAddModalViewController.swift
+//  BeBrav
+//
+//  Created by 공지원 on 22/01/2019.
+//  Copyright © 2019 bumslap. All rights reserved.
+//
+
+import UIKit
+
+class ArtAddViewController: UIViewController {
+    
+    //MARK : - Properties
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "작품 등록하기"
+        label.font = UIFont.boldSystemFont(ofSize: 40)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let artImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "add-image"))
+        imageView.contentMode = .scaleToFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let artNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "작품명을 입력해주세요."
+        textField.borderStyle = .roundedRect
+        textField.clearButtonMode = .whileEditing
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    lazy var imagePicker: UIImagePickerController = {
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        return picker
+    }()
+    
+    //MARK : - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(artImageView)
+        view.addSubview(titleLabel)
+        view.addSubview(artNameTextField)
+        
+        setUpLayout()
+        
+        //artImageView에 tapGesture를 추가
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(artImageViewTapped))
+        artImageView.isUserInteractionEnabled = true
+        artImageView.addGestureRecognizer(tapGestureRecognizer)
+        
+        artNameTextField.delegate = self
+    }
+    
+    //MARK : - Helper Method
+    @objc func artImageViewTapped() {
+        imagePicker.allowsEditing = true
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func setUpLayout() {
+        titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 120).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        
+        artImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        //artImageView.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor).isActive = true
+        artImageView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 30).isActive = true
+        //artImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        artImageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7).isActive = true
+        //artImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        artImageView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.3).isActive = true
+        
+        artNameTextField.topAnchor.constraint(equalTo: artImageView.bottomAnchor, constant: 20).isActive = true
+        artNameTextField.leadingAnchor.constraint(equalTo: artImageView.leadingAnchor).isActive = true
+        artNameTextField.trailingAnchor.constraint(equalTo: artImageView.trailingAnchor).isActive = true
+    }
+}
+
+extension ArtAddViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let editedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+        artImageView.image = editedImage
+        
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension ArtAddViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        artNameTextField.resignFirstResponder()
+    }
+}
---
+++ BeBrav/ArtistDetailCollectionViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ArtistDetailCollectionViewController.swift
+//  BeBrav
+//
+//  Created by 공지원 on 22/01/2019.
+//  Copyright © 2019 bumslap. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class ArtistDetailCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    
+        // Configure the cell
+    
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionView.elementKindSectionHeader {
+            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "HeaderCollectionReusableView", for: indexPath)
+            return headerView
+        }
+        return .init()
+    }
+    
+    /*
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let viewController = ArtAddViewController()
+        self.navigationController?.pushViewController(viewController, animated: false)
+    }
+     */
+}
