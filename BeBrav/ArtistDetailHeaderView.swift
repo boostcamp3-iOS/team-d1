f9bf5a131977e7d608d50dc14b6f142603d97eb2
gongjiwon <rag9008@naver.com>
2019-01-25 11:29:52 +0900

    no message

---
+++ BeBrav/ArtistDetailHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  ArtistDetailHeaderView.swift
+//  BeBrav
+//
+//  Created by 공지원 on 25/01/2019.
+//  Copyright © 2019 bumslap. All rights reserved.
+//
+
+import UIKit
+
+class ArtistDetailHeaderView: UICollectionReusableView {
+        
+}
