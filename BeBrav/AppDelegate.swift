320e7dcdfd789f75195c68c5f8205e616a19146a
bumslap <kl9151@naver.com>
2019-01-28 16:24:37 +0900

    for PT

--- BeBrav/AppDelegate.swift
+++ BeBrav/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let mainViewController = MainAutoResizingCollectionViewController(collectionViewLayout: MostViewedArtworkFlowLayout())
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = UIColor.white
+        window?.rootViewController = UINavigationController(rootViewController: mainViewController)
+        window?.makeKeyAndVisible()
         return true
     }
 
