8fb95a568e9756585b1ff3612319e066a5f29ee2
Seonghun Kim <kim.seonghun23@gmail.com>
2019-01-28 10:47:31 +0900

    xUnique 수정 적용

---
+++ BeBrav/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  BeBrav
+//
+//  Created by bumslap on 22/01/2019.
+//  Copyright © 2019 bumslap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
