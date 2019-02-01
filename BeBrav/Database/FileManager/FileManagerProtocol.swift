2f83b78a83750ecb35b3c0c85b7ca109ba659fe8
Kim Seonghun <40361234+Seonghun23@users.noreply.github.com>
2019-02-01 18:42:38 +0900

    SQLite Wrapper 및 이미지 저장을 위한 FileManager Protocol 추가 (#35)
    * SQLite Wrapper추가, FileManager Protocol 추가하여 이미지 저장 기능 확장, 의존성 주입 위해 SQLite Wrapper와 FileManager의 Stub 객체 추가
    
    * 불필요한 구문 삭제, CRUD는  String으로 변경,  Error를 costomNSError로 확장, Document의 이미지 저장 및 읽기 기능 별도 프로토콜로 분리, SQLite 자료형 관련 오류 수정
    
    * 코드 통일성 적용, 라인 정리

---
+++ BeBrav/Database/FileManager/FileManagerProtocol.swift
@@ -0,0 +1,32 @@
+//
+//  FileManagerProtocol.swift
+//  BeBrav
+//
+//  Created by Seonghun Kim on 29/01/2019.
+//  Copyright © 2019 bumslap. All rights reserved.
+//
+
+import UIKit
+
+// MARK:- FileManager Protocol
+protocol FileManagerProtocol {
+    func url(for directory: FileManager.SearchPathDirectory,
+             in domain: FileManager.SearchPathDomainMask,
+             appropriateFor url: URL?,
+             create shouldCreate: Bool) throws -> URL
+    
+    func urls(for directory: FileManager.SearchPathDirectory,
+              in domainMask: FileManager.SearchPathDomainMask) -> [URL]
+    
+    func fileExists(atPath path: String) -> Bool
+    
+    func createDirectory(atPath path:String,
+                         withIntermediateDirectories createIntermediates: Bool,
+                         attributes: [FileAttributeKey : Any]?) throws
+    
+    func createFile(atPath path: String,
+                    contents data: Data?,
+                    attributes attr: [FileAttributeKey : Any]?) -> Bool
+    
+    func removeItem(atPath path: String) throws
+}
