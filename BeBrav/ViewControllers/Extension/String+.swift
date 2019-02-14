16465c294aad0b6d2c577b82748f135451966d8d
Seonghun Kim <kim.seonghun23@gmail.com>
2019-02-14 17:31:22 +0900

    지역화 기본 설정 추가 및 영문, 한글, 아랍어 1차 번역 추가

---
+++ BeBrav/ViewControllers/Extension/String+.swift
@@ -0,0 +1,15 @@
+//
+//  String+.swift
+//  BeBrav
+//
+//  Created by Seonghun Kim on 14/02/2019.
+//  Copyright © 2019 bumslap. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
+}
